99ac2b135d60e656cc621845bc0b15bb186d451b
Kyungryul lee <leekyung1111@gmail.com>
2023-09-05 08:35:25 +0900

    fix : 병원 조회기능 보완
    - 병원 조회 버튼 기능 보완
    
    Ref #001

--- pcrHospital/src/main/java/com/example/pcrhospital/domain/HospitalRepository.java
+++ pcrHospital/src/main/java/com/example/pcrhospital/domain/HospitalRepository.java
@@ -8,12 +8,28 @@ import org.springframework.data.repository.query.Param;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
 
+    /**
+     * 시도명, 시군구명으로 병원 찾기
+     * @param sidoNm
+     * @param sgguNm
+     * @return
+     */
     @Query(value = "SELECT * FROM hospital where sidoNm = :sidoNm and sgguNm = :sgguNm and dgmPrscPsblYn = 'Y'", nativeQuery = true)
     List<Hospital> mFindHospital(@Param("sidoNm") String sidoNm, @Param("sgguNm") String sgguNm);
 
+    /**
+     * 시도명 목록 불러오기
+     * @param sidoNm
+     * @return
+     */
     @Query(value = "SELECT distinct sidoNm FROM hospital order by sidoNm", nativeQuery = true)
     List<String> mFindSidoNm();
 
+    /**
+     * 시군구명 목록 불러오기
+     * @param sidoNm
+     * @return
+     */
     @Query(value = "SELECT distinct sgguNm FROM hospital where sidoNm = :sidoNm order by sgguNm", nativeQuery = true)
     List<String> mFindSgguNm(@Param("sidoNm") String sidoNm);
 }
