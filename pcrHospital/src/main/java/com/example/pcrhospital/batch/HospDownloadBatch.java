2a73ccb7a091ffcb143c0a4849bfa9a11fdda201
Kyungryul lee <leekyung1111@gmail.com>
2023-09-04 15:35:01 +0900

    fix : 조회된 공공데이터를 저장
    - 조회된 공공데이터를 저장
    
    Ref #001

--- pcrHospital/src/main/java/com/example/pcrhospital/batch/HospDownloadBatch.java
+++ pcrHospital/src/main/java/com/example/pcrhospital/batch/HospDownloadBatch.java
@@ -1,7 +1,19 @@
 package com.example.pcrhospital.batch;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.pcrhospital.domain.Hospital;
+import com.example.pcrhospital.domain.HospitalRepository;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * <pre>
@@ -9,12 +21,72 @@ import org.springframework.stereotype.Component;
  * 병원이 추가된다면 추가된 병원만 db에 저장하도록 하는것이 복잡하기 때문에 기존 데이터를 삭제하고 전체를 다운로드해서 db에 저장한다.
  * </pre>
  */
+@RequiredArgsConstructor
 @Component
 public class HospDownloadBatch {
 
+    private final HospitalRepository hospRepository;
+
+    @Value("${api.key}")
+    String serviceKey;
+
     // 초 분 시 일 월 주
-    @Scheduled(cron = "0 * * * * *")
-    public void startBatch() {
-        System.out.println("download");
+    @Scheduled(cron = "0 34 * * * *")
+    public void startBatch() throws URISyntaxException {
+        List<Hospital> hospitals = new ArrayList<>();
+
+        RestTemplate rt = new RestTemplate();
+
+        int totalCount = 2;
+        String url = "https://apis.data.go.kr/B551182/rprtPtntDiagCnfcInfoService1/getRprtPtntDiagCnfcInfo1?" +
+                "serviceKey="+ serviceKey +
+                "&pageNo=1" +
+                "&_type=json" +
+                "&numOfRows=";
+
+        ResponseDto totalCntDto = rt.getForObject(new URI(url+totalCount), ResponseDto.class);
+        totalCount = totalCntDto.getResponse().getBody().getTotalCount();
+
+
+        ResponseDto responseDto = rt.getForObject(new URI(url+totalCount), ResponseDto.class);
+
+        List<Item> items = responseDto.getResponse().getBody().getItems().getItem();
+
+        System.out.println("totalCount = " + items.size());
+
+        items.stream().map(item -> {
+            Hospital hospital = new Hospital();
+            hospital.setYkiho(item.getYkiho());
+            hospital.setAddr(item.getAddr());
+            hospital.setCv19ExmTyCd(item.getCv19ExmTyCd());
+            hospital.setDgmPrscPsblYn(item.getDgmPrscPsblYn());
+            hospital.setYadmNm(item.getYadmNm());
+            hospital.setDiagBknPsblTelno(item.getDiagBknPsblTelno());
+            hospital.setInfcPtntDiagTyCd(item.getInfcPtntDiagTyCd());
+            hospital.setOnstpMadmYn(item.getOnstpMadmYn());
+            hospital.setRprtPtntDiagPsblYn(item.getRprtPtntDiagPsblYn());
+            hospital.setSidoNm(item.getSidoNm());
+            hospital.setSgguNm(item.getSgguNm());
+            return hospital;
+        }).forEach(hospitals::add);
+
+//        hospitals = items.stream().map((e) -> {
+//            return Hospital.builder()
+//                    .addr(e.getAddr())
+//                    .cv19ExmTyCd(e.getCv19ExmTyCd())
+//                    .dgmPrscPsblYn(e.getDgmPrscPsblYn())
+//                    .diagBknPsblTelno(e.getDiagBknPsblTelno())
+//                    .infcPtntDiagTyCd(e.getInfcPtntDiagTyCd())
+//                    .onstpMadmYn(e.getOnstpMadmYn())
+//                    .infcPtntDiagTyCd(e.getInfcPtntDiagTyCd())
+//                    .onstpMadmYn(e.getOnstpMadmYn())
+//                    .rprtPtntDiagPsblYn(e.getRprtPtntDiagPsblYn())
+//                    .sgguNm(e.getSgguNm())
+//                    .sidoNm(e.getSidoNm())
+//                    .yadmNm(e.getYadmNm())
+//                    .ykiho(e.getYkiho())
+//                    .build();
+//        }).collect(Collectors.toList());
+        hospRepository.saveAll(hospitals);
     }
 }
